e1a9730ae96af3bcc01e6f2937b0fd230316b731
batsdk <shemilkaweesha@gmail.com>
2025-03-16 12:20:15 +0530

    Chore: Generate all modules services and controllers

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT!, 10) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || '123456',
+      database: process.env.DB_NAME || 'taskdb',
+      autoLoadEntities: true,
+      synchronize: true, // Development Only
+    }),
+    TasksModule,
+    AuthModule,
+    UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
