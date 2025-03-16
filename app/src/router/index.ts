3adffef3b466c7d13edade251642e7cd628ea9a9
batsdk <shemilkaweesha@gmail.com>
2025-03-16 19:29:08 +0530

    Feature: add router guard

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
+import { isAuthenticated } from '@/utils/cookies'
+
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,16 +11,24 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        requiresAuth: true,
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/auth/login',
+      name: 'login',
+      component: () => import("@/views/LoginView.vue"),
     },
   ],
+})
+
+router.beforeEach((to,_,next) => {
+  if(to.meta.requiresAuth && !isAuthenticated()){
+    next('auth/login');
+  } else {
+    next();
+  }
 })
 
 export default router
---
+++ app/src/utils/cookies.ts
@@ -0,0 +1,16 @@
+export function getCookie(name: string) {
+  const cookies = document.cookie.split("; ");
+  for (let cookie of cookies) {
+    const [key, value] = cookie.split("=");
+    if (key === name) return decodeURIComponent(value);
+  }
+  return null;
+}
+
+export function isAuthenticated() {
+  return !!getCookie("access_token"); // Returns true if token exists
+}
+
+export function deleteCookie(name: string) {
+  document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
+}
