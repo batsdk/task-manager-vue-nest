dfc03aa2b3dc8b011d9c53647e13a7bd57caf1c9
batsdk <shemilkaweesha@gmail.com>
2025-03-16 18:19:58 +0530

    Chore: Update configuration

--- server/src/config/configuration.ts
+++ server/src/config/configuration.ts
@@ -8,8 +8,8 @@ export default () => ({
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT!, 10) || 5432,
     username: process.env.DB_USERNAME || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
-    name: process.env.DB_NAME || 'task_management',
+    password: process.env.DB_PASSWORD || '123456',
+    name: process.env.DB_NAME || 'taskdb',
     synchronize: process.env.DB_SYNCHRONIZE === 'true' || true,
   },
 });
