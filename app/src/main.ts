31789347186fa5ed41e526a549b9175e120017a1
batsdk <shemilkaweesha@gmail.com>
2025-03-18 14:49:41 +0530

    Chore: Set up vuestic

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,14 +2,18 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createVuestic } from 'vuestic-ui'
 
 import App from './App.vue'
 import router from './router'
 import "./index.css"
+import 'vuestic-ui/styles/essential.css';
+import 'vuestic-ui/styles/typography.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(createVuestic())
 
 app.mount('#app')
--- app/src/views/LoginView.vue
+++ app/src/views/LoginView.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold underline" >Login Page Here</h1>
+    <VaButton>Button</VaButton>
   </div>
 </template>
